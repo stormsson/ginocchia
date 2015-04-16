8ac3b470557f6cd17b426135f4eaca09664e1f8f
Camillo Camarda <ccamarda@h-art.com>
2015-04-16 16:22:39 +0200

    db e readme

---
+++ data/database.sql
@@ -0,0 +1,31 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.9
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Apr 16, 2015 at 04:21 PM
+-- Server version: 5.6.16
+-- PHP Version: 5.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `ginocchia`
+--
+CREATE DATABASE IF NOT EXISTS `ginocchia` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `ginocchia`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
+
+DROP TABLE IF EXISTS `events`;
+CREATE TABLE IF NOT EXISTS `events` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
